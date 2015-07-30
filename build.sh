3f290ea23213c88a56900d15de31cf94293808ae
Tim Baumann <tim@timbaumann.info>
2015-07-30 15:26:52 +0200

    Literaturverzeichnis hinzugefügt

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 set -e
+
+# siehe auch http://tex.stackexchange.com/a/12717
+mkdir -p output
+cp literatur.bib output
+cd output; bibtex spektralsequenzen_serre; cd ..
 pdflatex -output-directory output spektralsequenzen_serre.tex
 cp output/spektralsequenzen_serre.pdf .
