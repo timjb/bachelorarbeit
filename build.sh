e99fb735ca55aa50196242f01843a9cd38d84bc4
Tim Baumann <tim@timbaumann.info>
2015-08-20 14:12:56 +0200

    spektralsequenzen_serre -> homotopiegruppen_serre

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-# siehe auch http://tex.stackexchange.com/a/12717
-if [ ! -f output/spektralsequenzen_serre.aux ]; then
-  pdflatex -output-directory output spektralsequenzen_serre.tex
-fi
 mkdir -p output
+# siehe auch http://tex.stackexchange.com/a/12717
+if [ ! -f output/homotopiegruppen_serre.aux ]; then
+  pdflatex -output-directory output homotopiegruppen_serre.tex
+fi
 cp literatur.bib output
-cd output; bibtex spektralsequenzen_serre; cd ..
-pdflatex -output-directory output spektralsequenzen_serre.tex
-cp output/spektralsequenzen_serre.pdf .
+cd output; bibtex homotopiegruppen_serre; cd ..
+pdflatex -output-directory output homotopiegruppen_serre.tex
+cp output/homotopiegruppen_serre.pdf .
